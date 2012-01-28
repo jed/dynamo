e93af0bf0768aa25e2e9179a7b6052b132eacd68
Jed Schmidt <tr@nslator.jp>
2012-01-28 20:42:07 +0900

    debug travis

--- test/fetch_credentials.js
+++ test/fetch_credentials.js
@@ -6,23 +6,23 @@ var https = require("https")
 module.exports = function(cb) {
   var json = ""
 
-  console.log(process.env)
-
-  throw new Error
-
   if (process.env.TRAVIS) {
+    console.log("fetching remote")
     https.get({host: credentialHost}, function(res) {
       res.on("data", function(chunk){ json += chunk })
       res.on("end", parse)
     }).on("error", cb)    
   }
 
-  else fs.readFile(__dirname + "/credentials.json", "utf8", function(err, data) {
-    if (err) return cb(err)
+  else {
+    console.log("fetching locale")
+    fs.readFile(__dirname + "/credentials.json", "utf8", function(err, data) {
+      if (err) return cb(err)
 
-    json = data
-    parse()
-  })
+      json = data
+      parse()
+    })
+  }
 
   function parse() {
     var data
