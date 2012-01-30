06a9e408e9100727b2625bb4b0185a26782347b8
Jed Schmidt <tr@nslator.jp>
2012-01-30 21:50:51 +0900

    got multiple envd working, remove debug
    [ci skip]

--- test/setup.js
+++ test/setup.js
@@ -2,8 +2,6 @@ var should = require("should")
   , dynamo = require("../")
   , db = dynamo.createClient()
 
-console.log(process.env)
-
 describe("setup", function() {
   it("delete any existing tables", function(done) {
     var table1 = {TableName: "DYNAMO_TEST_TABLE_1"}
