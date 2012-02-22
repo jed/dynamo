3d6e22a7ed3c6c7d1b147686f4b7058955e6c1ed
Jed Schmidt <tr@nslator.jp>
2012-02-23 00:17:03 +0900

    0.2.0

--- test/teardown.js
+++ test/teardown.js
@@ -2,30 +2,20 @@ var should = require("should")
   , dynamo = require("../")
   , db = dynamo.createClient()
 
-  , table1 = new db.Table("DYNAMO_TEST_TABLE_1")
-  , table2 = new db.Table("DYNAMO_TEST_TABLE_2")
-
 describe("teardown -", function() {
-  it("delete any existing tables", function(done) {
-    table1.destroy(function(err, table) {
-      should.not.exist(err)
-      should.exist(table)
-
-      table.should.have.property("TableStatus", "DELETING")
-
-      table2.destroy(done)
+  it("delete existing test tables", function(done) {
+    db.remove("DYNAMO_TEST_TABLE_1", function() {
+      db.remove("DYNAMO_TEST_TABLE_2", function() {
+	done()
+      })
     })
   })
 
-  it("make sure no tables exist", function check1(done) {
-    table1.fetch(function(err) {
-      if (!err) setTimeout(check1, 5000, done)
-
-      else !function check2() {
-        table2.fetch(function(err) {
-          err ? done() : setTimeout(check2, 5000, done)
-        })
-      }()
+  it("make sure no test tables exist", function(done) {
+    db.get("DYNAMO_TEST_TABLE_1").watch(function() {
+      db.get("DYNAMO_TEST_TABLE_2").watch(function() {
+	done()
+      })
     })
   })
 })
